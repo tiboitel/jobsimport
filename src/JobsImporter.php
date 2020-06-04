1a78ba561260682f5007307d4f614b917a7c092e
r-gautier <renaud.gautier@gadz.org>
2020-06-04 16:08:27 +0200

    Add app to jobsimport

---
+++ src/JobsImporter.php
@@ -0,0 +1,44 @@
+<?php
+
+class JobsImporter
+{
+    private $db;
+
+    private $file;
+
+    public function __construct($host, $username, $password, $databaseName, $file)
+    {
+        $this->file = $file;
+        
+        /* connect to DB */
+        try {
+            $this->db = new PDO('mysql:host=' . $host .';dbname=' . $databaseName, $username, $password);
+        } catch (Exception $e) {
+            die('DB error: ' . $e->getMessage() . "\n");
+        }
+    }
+
+    public function importJobs()
+    {
+        /* remove existing items */
+        $this->db->exec('DELETE FROM job');
+
+        /* parse XML file */
+        $xml = simplexml_load_file($this->file);
+
+        /* import each item */
+        $count = 0;
+        foreach ($xml->item as $item) {
+            $this->db->exec('INSERT INTO job (reference, title, description, url, company_name, publication) VALUES ('
+                . '\'' . addslashes($item->ref) . '\', '
+                . '\'' . addslashes($item->title) . '\', '
+                . '\'' . addslashes($item->description) . '\', '
+                . '\'' . addslashes($item->url) . '\', '
+                . '\'' . addslashes($item->company) . '\', '
+                . '\'' . addslashes($item->pubDate) . '\')'
+            );
+            $count++;
+        }
+        return $count;
+    }
+}
---
+++ src/JobsLister.php
@@ -0,0 +1,22 @@
+<?php
+
+class JobsLister
+{
+    private $db;
+
+    public function __construct($host, $username, $password, $databaseName)
+    {
+        /* connect to DB */
+        try {
+            $this->db = new PDO('mysql:host=' . $host .';dbname=' . $databaseName, $username, $password);
+        } catch (Exception $e) {
+            die('DB error: ' . $e->getMessage() . "\n");
+        }
+    }
+
+    public function listJobs()
+    {
+        $jobs = $this->db->query('SELECT id, reference, title, description, url, company_name, publication FROM job')->fetchAll(PDO::FETCH_ASSOC);
+        return $jobs;
+    }
+}
